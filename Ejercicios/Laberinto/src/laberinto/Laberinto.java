3df58e8f1580957ce5b81d445946d59376d4b611
DuvanTique <dtiqueo@unal.edu.co>
2022-11-05 17:47:31 -0500

    Carga inicial

---
+++ Ejercicios/Laberinto/src/laberinto/Laberinto.java
@@ -0,0 +1,18 @@
+package laberinto;
+
+import laberinto.view.VistaLaberinto;
+
+/**
+ *
+ * @author Duvan 
+ */
+public class Laberinto {
+
+
+    public static void main(String[] args) {
+        VistaLaberinto principal = new VistaLaberinto();
+        principal.setVisible(true);
+        principal.setLocationRelativeTo(null);
+    }
+    
+}
